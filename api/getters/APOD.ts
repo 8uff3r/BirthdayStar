c4d11ff24d20a7f186b042beb43b8f904325ffbe
8uff3r <8uff3r@gmail.com>
2023-07-03 11:54:40 +0330

    add got

--- api/getters/APOD.ts
+++ api/getters/APOD.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
 import { load } from "cheerio";
+import got from "got";
 
 export const getApod = async () => {
-  const res = await axios.get("https://apod.nasa.gov/apod/astropix.html");
-  const $ = load(res.data, null, false);
+  const res = await got.get("https://apod.nasa.gov/apod/astropix.html");
+  const $ = load(res.body, null, false);
   const Apod = {
     name: $("b").first().html()?.trim(),
     desc: $("p").eq(2).not("b").text()?.replace("Explanation: ", "").replaceAll("\n", " ").trim(),
